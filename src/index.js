7e5042a5cc507e862e6ab7fa7a37063dc44d0eba
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-25 21:40:55 +0200

    Deleted unneccessary imports

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import WeatherWrap from "./Components/WeatherWrap/WeatherWrap";
-import ListItem from "./Components/ListItem/ListItem";
-
-
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
