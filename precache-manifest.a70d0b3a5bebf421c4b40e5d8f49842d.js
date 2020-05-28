53c5f265088be6693d9a148401e8bfa6df252c9b
Zionknight-crypto <info.davidhauser@gmail.com>
2020-05-28 15:09:33 +0300

    Updates

---
+++ precache-manifest.a70d0b3a5bebf421c4b40e5d8f49842d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87129f313bc3adcd8e178b9d8bcd5b75",
+    "url": "/React-team-project-Weather/index.html"
+  },
+  {
+    "revision": "4a372ca2ca0fc6bb9d3f",
+    "url": "/React-team-project-Weather/static/css/main.e81243ea.chunk.css"
+  },
+  {
+    "revision": "9c9b4810c26b60efc3fe",
+    "url": "/React-team-project-Weather/static/js/2.b78099b8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-team-project-Weather/static/js/2.b78099b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a372ca2ca0fc6bb9d3f",
+    "url": "/React-team-project-Weather/static/js/main.6dd734a7.chunk.js"
+  },
+  {
+    "revision": "e0dc34f948f8c224203d",
+    "url": "/React-team-project-Weather/static/js/runtime-main.36f508b6.js"
+  }
+]);
